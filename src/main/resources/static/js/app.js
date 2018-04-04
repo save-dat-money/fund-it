e864f2733b7eff62d89052977385e12b1081c2a8
Jallappino <stefan.murakami@gmail.com>
2018-04-04 09:48:32 -0400

    wednesday morning

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,47 @@
+const xhr = new XMLHttpRequest()
+xhr.onreadystatechange = function() {
+	if (xhr.readyState === 4 && xhr.status === 200) {
+		const res = JSON.parse(xhr.response);
+
+		if(res.length) {
+			res.forEach(function(account) {
+				appendOneElementToBody(account)
+			})
+		} else {
+			appendOneElementToBody(res)
+		}
+
+		fuction appendOneElementToBody(res) {
+			const body = document.body
+
+			const accountContainer = document.createElement('div')
+			accountContainer.classList.add('accountContainer')
+
+			appendElement(accountContainer, createElement('h2', res.balance))
+
+			appendElement(body, accountContainer)
+		}
+
+		function createElement(elem, textValue) {
+			const newElem = document.createElement(elem)
+			newElem.innerText = textValue
+
+			return newElem
+		}
+
+		function appendElement(parent, child) {
+			parent.appendChild(child)
+		}
+
+		function showAllPropsInObject(object) {
+			for (prop in res) {
+				console.log(`${prop} ${res[prop]}`)
+			}
+		}
+
+		console.log(res)
+	}
+}
+
+xhr.open('GET', 'htp://localhost:8080/index', true)
+xhr.send()
