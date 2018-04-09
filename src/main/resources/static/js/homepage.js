fe063716bc2bc5361865396ab34013bbfbdeb512
Alex <wcadidas15@gmail.com>
2018-04-09 14:39:25 -0400

    Put the pie chart in the wood

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -7,3 +7,46 @@ const toggleMenu = () => {
 };
 
 toggleMenu()
+
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+
+      const data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     10],
+          ['Commute',  2],
+          ['Eat',      2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        const options = {
+          title: 'My Daily Activities',
+          backgroundColor: { fill: 'transparent'},
+          colors: [
+            '#6A99CB',
+            '#F27370',
+            '#FA9856',
+            '#ACBD86',
+            '#F7B32D'
+            ]
+        };
+     
+       
+      const chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      
+google.visualization.events.addListener(chart, 'select', selectHandler);
+
+function selectHandler() {
+  var selection = chart.getSelection();
+  const otherside = document.querySelector('.otherside');
+  const value = data.getValue(selection[0].row || 0, selection[0].column || 0); 
+  var t = document.createTextNode(value);
+  otherside.replaceChild(t, otherside.childNodes[0]);
+}
+      }
+      
