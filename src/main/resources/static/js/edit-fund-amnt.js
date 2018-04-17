2ca659eaaa8a3216bb599d6f3d24d08f3e2ac93b
Alex <wcadidas15@gmail.com>
2018-04-17 13:56:19 -0400

    Fixed toggling

--- src/main/resources/static/js/edit-fund-amnt.js
+++ src/main/resources/static/js/edit-fund-amnt.js
@@ -21,15 +21,13 @@ function editFundAmnt(event) {
     	if (xhrFundIncrease.readyState === 4 && xhrFundIncrease.status === 200) {
             console.log(xhrFundIncrease)
             const res = JSON.parse(xhrFundIncrease.response)
-            let editFundAmntModal = document.querySelector(".edit-fund-amount-modal")
-            editFundAmntModal.classList.toggle("show-edit-fund-modal");
+            toggleEditFundAmntModal();
             console.log(res.fundAmount)
             document.querySelector('#fundAmountBefore').innerHTML = "Balance: " + res.fundAmount.toFixed(2);
 			document.querySelector('#unassigned_funds_in_fund').innerHTML = "Unassigned Funds Available: " + res.account.unassignedFundAmount.toFixed(2);
 			fundsApp.refreshFunds( () => {
 				drawChart(fundsApp.funds)
 			});
-			toggleEditFundAmntModal();
         }
 
     } 
@@ -54,15 +52,12 @@ function decrFundAmnt(event) {
     	if (xhrFundDecrease.readyState === 4 && xhrFundDecrease.status === 200) {
             console.log(xhrFundDecrease)
             const res = JSON.parse(xhrFundDecrease.response)
-            let decrFundAmntModal = document.querySelector(".decrease-fund-amount-modal")
-            decrFundAmntModal.classList.toggle("show-edit-fund-modal");
-            console.log(res.fundAmount)
+            toggleDecrFundAmntModal();
             document.querySelector('#fundAmountBefore').innerHTML = "Balance: " + res.fundAmount.toFixed(2);
 			document.querySelector('#unassigned_funds_in_fund').innerHTML = "Unassigned Funds Available: " + res.account.unassignedFundAmount.toFixed(2);
 			fundsApp.refreshFunds( () => {
 				drawChart(fundsApp.funds)
 			});
-			toggleDecrFundAmntModal();
         }
 
     }
