9fbd59c7b33c4c63923826f5f1a93cbfa8762c7a
JacobKaylor <jkaylor20@yahoo.com>
2018-04-19 13:45:04 -0400

    populator change

--- src/main/java/org/wecancodeit/columbus/fundit/AccountPopulator.java
+++ src/main/java/org/wecancodeit/columbus/fundit/AccountPopulator.java
@@ -16,16 +16,13 @@ public class AccountPopulator implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Account account = new Account("Account 1", 1000);
+		Account account = new Account("My Account", 3000);
 		accountRepo.save(account);
 		
-		Account account2 = new Account("Account 2", 1000);
-		accountRepo.save(account2);
-		
-		Fund fund = new Fund("Test 1", account, 200);
+		Fund fund = new Fund("Emergency", account, 1000);
 		fundRepo.save(fund);
 		
-		Fund fund2 = new Fund("Test 2", account, 300);
+		Fund fund2 = new Fund("Car", account, 1000);
 		fundRepo.save(fund2);
 		
 //		Fund fund3 = new Fund("Test Fund", account2, 400);
