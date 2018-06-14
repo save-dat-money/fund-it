dcc098fba2775a81773a13d07d48a183122b732c
Japapino <stefan.murakami@gmail.com>
2018-06-14 14:13:47 -0500

    first iteration of testing for login backend finished

--- src/main/java/org/wecancodeit/columbus/fundit/AccountRestController.java
+++ src/main/java/org/wecancodeit/columbus/fundit/AccountRestController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.Map;
-//import sun.misc.Request;
 
 @RestController
 public class AccountRestController {
--- src/main/java/org/wecancodeit/columbus/fundit/LoginAuthenticationController.java
+++ src/main/java/org/wecancodeit/columbus/fundit/LoginAuthenticationController.java
@@ -1,5 +1,7 @@
 package org.wecancodeit.columbus.fundit;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,23 +9,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-//@RestController
+@Controller
 public class LoginAuthenticationController {
 
 	@Resource
 	LoginRepository loginRepo;
 
-	@RequestMapping(path = "/login/{username}/{passwordInput}", method = RequestMethod.PUT)
-	public boolean authenticate(@PathVariable("username") String username,
-			@PathVariable("passwordInput") String passwordInput) {
-
-		Login checkName = loginRepo.findByUsername(username);
-		if (checkName != null) {
-			checkName.checkPassword(passwordInput);
-			return true; 
-		}
-		
-		return false; 
+	
+	@RequestMapping(value = "Login")
+	public String getAllReviews(Model model, Model model1) {
+		return "Login";
 	}
+	
+//	@RequestMapping(path = "/login/{username}/{passwordInput}", method = RequestMethod.PUT)
+//	public boolean authenticate(@PathVariable("username") String username,
+//			@PathVariable("passwordInput") String passwordInput) {
+//
+//		Login checkName = loginRepo.findByUsername(username);
+//		if (checkName != null) {
+//			checkName.checkPassword(passwordInput);
+//			return true; 
+//		}
+//		
+//		return false; 
+//	}
 
 }
