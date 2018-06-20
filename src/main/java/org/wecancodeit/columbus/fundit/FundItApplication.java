fc0f33b56e199b943e7eb5f53138ca674be4cf10
Jallippino <35497750+Japapino@users.noreply.github.com>
2018-06-20 10:48:37 -0500

    added config file

--- src/main/java/org/wecancodeit/columbus/fundit/FundItApplication.java
+++ src/main/java/org/wecancodeit/columbus/fundit/FundItApplication.java
@@ -2,6 +2,9 @@ package org.wecancodeit.columbus.fundit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 
 @SpringBootApplication
 public class FundItApplication {
