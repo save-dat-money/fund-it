3122c9ddf0a325900552a61b31597c10d3f831fa
Jallippino <35497750+Japapino@users.noreply.github.com>
2018-06-28 20:46:56 -0500

    initial setup

--- src/main/java/org/wecancodeit/columbus/fundit/FundItApplication.java
+++ src/main/java/org/wecancodeit/columbus/fundit/FundItApplication.java
@@ -2,8 +2,8 @@ package org.wecancodeit.columbus.fundit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
 @SpringBootApplication
--- src/main/java/org/wecancodeit/columbus/fundit/config/SecurityConfiguration.java
+++ src/main/java/org/wecancodeit/columbus/fundit/config/SecurityConfiguration.java
@@ -1,30 +1,30 @@
 package org.wecancodeit.columbus.fundit.config;
 
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@EnableWebSecurity
-@Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
-
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-
-
-        httpSecurity
-                .authorizeRequests()
-//                .antmatchers("**/rest/*") //for authorizing certain requests
-                .anyRequest()
-                .permitAll()
-                .and()
-                //.addFilterBefore(customFilter(), BasicAuthenticationFilter.class)
-                .httpBasic();
-        httpSecurity.csrf().disable();
-    }
-
-}
+//@EnableWebSecurity
+//@Configuration
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+//
+//    @Override
+//    protected void configure(HttpSecurity httpSecurity) throws Exception {
+//
+//
+//        httpSecurity
+//                .authorizeRequests()
+////                .antmatchers("**/rest/*") //for authorizing certain requests
+//                .anyRequest()
+//                .permitAll()
+//                .and()
+//                //.addFilterBefore(customFilter(), BasicAuthenticationFilter.class)
+//                .httpBasic();
+//        httpSecurity.csrf().disable();
+//    }
+//
+//}
 
